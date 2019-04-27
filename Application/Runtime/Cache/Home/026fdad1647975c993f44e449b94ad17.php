63711c371c26c1672b08f9af52aa1ad646ddc5e8
ssy <ssy@ssygit.com>
2019-04-27 17:26:25 +0800

    修改home页名称

--- Application/Runtime/Cache/Home/026fdad1647975c993f44e449b94ad17.php
+++ Application/Runtime/Cache/Home/026fdad1647975c993f44e449b94ad17.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="<?php echo HomeCssUrl;?>/reset.css" />
     <link rel="stylesheet" href="<?php echo HomeCssUrl;?>/font-awesome.min.css" />
     <!-- <link rel="stylesheet" href="<?php echo HomeCssUrl;?>/aa.css" /> -->
-    <link rel="stylesheet" href="<?php echo HomeCssUrl;?>/indexn.css" />
+    <link rel="stylesheet" href="<?php echo HomeCssUrl;?>/videos.css" />
     <script type="text/javascript" src="<?php echo HomeJsUrl;?>/jquery-3.1.1.min.js"></script>
     <style>
 
@@ -34,15 +34,22 @@
 </div>
 <div class="videos-container">
     <div class="tabs">
-        <div class="title">品牌</div>
+        <div class="title active">品牌</div>
         <div class="title">产品</div>
         <div class="title">接线</div>
     </div>
-    <div class="content">
-        <div class="one">
-            <video id="video" poster="" controls="controls" src="<?php echo HomeCssUrl;?>/../vi.mp4"></video>
-            <div id="output"></div>
+    <div class="videos">
+        <div class="line videos">
+            <div class="one">
+                <video  poster="" controls="controls" src="<?php echo HomeCssUrl;?>/../vi.mp4"></video>
+                <div ></div>
+            </div>
+            <div class="one">
+                <video  poster="" controls="controls" src="<?php echo HomeCssUrl;?>/../vi.mp4"></video>
+                <div ></div>
+            </div>
         </div>
+
     </div>
 </div>
 
@@ -50,30 +57,31 @@
        controls="controls" src="http://ips.ifeng.com/video19.ifeng.com/video09/2017/03/02/2335458-102-009-091204
        .mp4?vid=cc2f2f25-e8af-407f-979c-99d0ac3cd4a3&amp;uid=1555558230049_mjqrk97023&amp;from=v_Free&amp;pver=vHTML5Player_v2.0.0&amp;sver=&amp;
        &lt;!&ndash;se=印度技术宅&amp;cat=140-141&amp;ptype=140&amp;platform=pc&amp;sourceType=h5&amp;dt=1484582400000&amp;gid=GlCSwWoufu4Q&amp;sign=60b35d3c004de26b2d5c3f5f6ff6c90d&amp;tm=1555558231594"></video>-->
+<script type="text/javascript" src="<?php echo HomeJsUrl;?>/videos.js"></script>
 <script type="text/javascript">
-    (function(){
-        var video, output;
-        var scale = 0.8;
-        var initialize = function() {
-            output = document.getElementById("output");
-            video = document.getElementById("video");
-            video.addEventListener('loadeddata',captureImage);
-        };
-
-        var captureImage = function() {
-            var canvas = document.createElement("canvas");
-            canvas.width = video.videoWidth * scale;
-            canvas.height = video.videoHeight * scale;
-            canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-
-            var img = document.createElement("img");
-            img.crossOrigin="anonymous";
-            img.src = canvas.toDataURL("image/png");
-            output.appendChild(img);
-        };
-
-        initialize();
-    })();
+//    (function(){
+//        var video, output;
+//        var scale = 0.8;
+//        var initialize = function() {
+//            output = document.getElementById("output");
+//            video = document.getElementById("video");
+//            video.addEventListener('loadeddata',captureImage);
+//        };
+//
+//        var captureImage = function() {
+//            var canvas = document.createElement("canvas");
+//            canvas.width = video.videoWidth * scale;
+//            canvas.height = video.videoHeight * scale;
+//            canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+//
+//            var img = document.createElement("img");
+//            img.crossOrigin="anonymous";
+//            img.src = canvas.toDataURL("image/png");
+//            output.appendChild(img);
+//        };
+//
+//        initialize();
+//    })();
 </script>
 </body>
 
--- Application/Runtime/Cache/Home/4add8af5b8497d93c563ff6d9edae109.php
+++ Application/Runtime/Cache/Home/4add8af5b8497d93c563ff6d9edae109.php
@@ -63,11 +63,6 @@
         <input type="text" class="pro" data-type="" data-id=""/>
     </div>
 </div>
-
-<!--<video poster=""
-       controls="controls" src="http://ips.ifeng.com/video19.ifeng.com/video09/2017/03/02/2335458-102-009-091204
-       .mp4?vid=cc2f2f25-e8af-407f-979c-99d0ac3cd4a3&amp;uid=1555558230049_mjqrk97023&amp;from=v_Free&amp;pver=vHTML5Player_v2.0.0&amp;sver=&amp;
-       &lt;!&ndash;se=印度技术宅&amp;cat=140-141&amp;ptype=140&amp;platform=pc&amp;sourceType=h5&amp;dt=1484582400000&amp;gid=GlCSwWoufu4Q&amp;sign=60b35d3c004de26b2d5c3f5f6ff6c90d&amp;tm=1555558231594"></video>-->
 <script type="text/javascript" src="<?php echo HomeJsUrl;?>/jquery-3.1.1.min.js"></script>
 <script type="text/javascript" src="<?php echo HomeJsUrl;?>/questions.js"></script>
 </body>
