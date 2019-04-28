929ae0de8131d2b59aac81e68f560c2cadd4dc7c
ssy <ssy@ssygit.com>
2019-04-29 02:10:50 +0800

    第一版意见修改

--- Application/Runtime/Cache/Home/0d68cee479ec467eadfc85bb99cfb029.php
+++ Application/Runtime/Cache/Home/0d68cee479ec467eadfc85bb99cfb029.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
 <html>
 
 <head>
@@ -84,14 +84,14 @@
                 <div id="fileList" class="uploader-list"></div>
 
 
-            </div>
-            <div style="clear: both"></div>
+            <!--</div>-->
+            <!--<div style="clear: both"></div>-->
+        <!--</div>-->
+        <div class="line">
+            <span class="label com-advices">*投诉建议</span>
+            <textarea class="com-advices" name="" id="" cols="30" rows="5"></textarea>
         </div>
-
-            <div class="line">
-                <span class="label com-advices">*投诉建议</span>
-                <textarea class="com-advices" name="" id="" cols="30" rows="5"></textarea>
-            </div>
+    </div>
     <button class="submit sub-mar">提交</button>
 
 <script type="text/javascript" src="<?php echo HomeJsUrl;?>/jquery-3.1.1.min.js"></script>
---
+++ Application/Runtime/Cache/Home/7384047cf8e1eb02e9011a0336c4abaa.php
@@ -0,0 +1,107 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0" />
+    <title>申请加盟</title>
+    <link rel="stylesheet" href="<?php echo HomeCssUrl;?>/reset.css" />
+    <link rel="stylesheet" href="<?php echo HomeCssUrl;?>/font-awesome.min.css" />
+    <!-- <link rel="stylesheet" href="<?php echo HomeCssUrl;?>/aa.css" /> -->
+    <link rel="stylesheet" href="<?php echo HomeCssUrl;?>/joinus.css" />
+
+    <style>
+
+
+        @media only screen and (min-width: 768px) {
+            input {
+                margin-top: 5px;
+            }
+        }
+
+
+
+
+
+    </style>
+</head>
+
+<body>
+<div class="logo-container">
+    <div class="logo">
+        <img src="<?php echo Homessy;?>/logo.png" alt="SIEMENS">
+    </div>
+</div>
+    <div class="hr"></div>
+    <div class="info-container">
+        <div class="line">
+            <span class="label name">姓名</span>
+            <input type="text" class="name">
+        </div>
+        <div class="line">
+            <span class="label tel">电话</span>
+            <input type="text" class="tel">
+        </div>
+        <div class="line">
+            <span class="label">省份</span>
+            <select onchange="chinaChange(this,document.getElementById('city'));" name="" id="province" class="province">
+                <option value ="请选择市区">请选择省份</option>
+                <option value ="北京市">
+
+                    北京市 </option><option value ="天津市">
+                天津市 </option><option value ="上海市">
+                上海市 </option><option value ="重庆市">
+                重庆市 </option><option value ="河北省">
+                河北省 </option><option value ="山西省">
+                山西省 </option><option value ="辽宁省">
+                辽宁省 </option><option value ="吉林省">
+                吉林省 </option><option value ="黑龙江省">
+                黑龙江省</option><option value ="江苏省">
+                江苏省 </option><option value ="浙江省">
+                浙江省 </option><option value ="安徽省">
+                安徽省 </option><option value ="福建省">
+                福建省 </option><option value ="江西省">
+                江西省 </option><option value ="山东省">
+                山东省 </option><option value ="河南省">
+                河南省 </option><option value ="湖北省">
+                湖北省 </option><option value ="湖南省">
+                湖南省 </option><option value ="广东省">
+                广东省 </option><option value ="海南省">
+                海南省 </option><option value ="四川省">
+                四川省 </option><option value ="贵州省">
+                贵州省 </option><option value ="云南省">
+                云南省 </option><option value ="陕西省">
+                陕西省 </option><option value ="甘肃省">
+                甘肃省 </option><option value ="青海省">
+                青海省 </option><option value ="台湾省">
+                台湾省 </option><option value ="广西壮族自治区">
+                广西壮族自治区</option><option value ="内蒙古自治区">
+                内蒙古自治区</option><option value ="西藏自治区">
+                西藏自治区</option><option value ="宁夏回族自治区">
+                宁夏回族自治区 </option><option value ="新疆维吾尔自治区">
+                新疆维吾尔自治区</option><option value ="香港特别行政区">
+                香港特别行政区</option><option value ="澳门特别行政区">
+                澳门特别行政区</option>
+            </select>
+            <span class="i-con"><i class="fa fa-caret-down fa-lg"></i></span>
+
+        </div>
+        <div class="line">
+            <span class="label">城市</span>
+            <select name="" id="city" class="city">
+                <option value ="请选择市区">请选择市区</option>
+            </select>
+            <span class="i-con"><i class="fa fa-caret-down fa-lg"></i></span>
+        </div>
+        <div class="line agency">
+            <span class="label" style="text-align: left">&nbsp;&nbsp;&nbsp;&nbsp;希望代理的产品</span>
+            <textarea name="" id="" cols="30" rows="5"></textarea>
+        </div>
+    </div>
+    <button class="submit">提交</button>
+
+<script type="text/javascript" src="<?php echo HomeJsUrl;?>/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="<?php echo HomeJsUrl;?>/joinus.js"></script>
+</body>
+
+</html>
